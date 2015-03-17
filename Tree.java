9d8cab2aa8a237a71f9da8ffebe16454fdee2c2a
kpedneka <kpednekar95@gmail.com>
2015-03-16 21:33:54 -0400

    version 1
    Trying out different ways to build a DOM Tree. So far, the only
    relevant file is Tree.java and the only method I have worked on is
    build(). Let’s see how the other ideas compare

---
+++ Tree.java
@@ -0,0 +1,175 @@
+package structures;
+
+import java.util.*;
+
+/**
+ * This class implements an HTML DOM Tree. Each node of the tree is a TagNode, with fields for
+ * tag/text, first child and sibling.
+ * 
+ */
+public class Tree {
+	
+	/**
+	 * Root node
+	 */
+	TagNode root=null;
+	
+	/**
+	 * Scanner used to read input HTML file when building the tree
+	 */
+	Scanner sc;
+	
+	/**
+	 * Initializes this tree object with scanner for input HTML file
+	 * 
+	 * @param sc Scanner for input HTML file
+	 */
+	public Tree(Scanner sc) {
+		this.sc = sc;
+		root = null;
+	}
+	
+	/**
+	 * Builds the DOM tree from input HTML file. The root of the 
+	 * tree is stored in the root field.
+	 */
+	public void build() {
+		/** COMPLETE THIS METHOD **/
+		Stack<TagNode> stk = new Stack<TagNode>();
+		
+		String temp = sc.nextLine();
+		
+		TagNode ptr = null, prev = null;
+		temp = temp.substring(1,temp.length()-1);
+		root = new TagNode (temp, null, null);
+		stk.push(root);
+		ptr = root;
+		temp = sc.nextLine();
+		temp = temp.substring(1, temp.length()-1);
+		root.firstChild = new TagNode (temp, null, null);
+		stk.push(root.firstChild);
+		prev = ptr;
+		ptr = root.firstChild;
+		
+		while (sc.hasNext()){
+			temp = sc.nextLine();
+			if (temp.charAt(0) != '<'){
+				//plain text
+				if (ptr.firstChild == null){
+					ptr.firstChild = new TagNode (temp, null, null);
+					prev = ptr;
+					ptr = ptr.firstChild;
+				} else {
+					prev = ptr;
+					ptr = ptr.firstChild;
+					while (ptr.sibling != null){
+						ptr = ptr.sibling;
+					}
+					ptr.sibling = new TagNode (temp, null, null);
+					ptr = ptr.sibling;
+				}
+			} else {
+				if (temp.charAt(1) == '/'){
+					//closing tag
+					ptr = stk.pop();
+				} else {
+					//new tag. This gives problems. Can't find a condition to decide where to add new tag
+					temp = temp.substring(1,temp.length()-1);
+					if (ptr == prev){ // just popped something
+						ptr.sibling = new TagNode (temp, null, null);
+						prev = ptr;
+						ptr = ptr.sibling;
+						stk.push(ptr);
+					} else {
+						if (prev.firstChild != null){ //adding a sibling
+							ptr.sibling = new TagNode (temp, null, null);
+							prev = ptr;
+							ptr = ptr.sibling;
+							stk.push(ptr);
+						} else { //adding a firstChild
+							ptr.firstChild = new TagNode (temp, null, null);
+							prev = ptr;
+							ptr = ptr.firstChild;
+							stk.push(ptr);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	
+	/**
+	 * Replaces all occurrences of an old tag in the DOM tree with a new tag
+	 * 
+	 * @param oldTag Old tag
+	 * @param newTag Replacement tag
+	 */
+	public void replaceTag(String oldTag, String newTag) {
+		/** COMPLETE THIS METHOD **/
+		TagNode ptr = root, prev = null;
+		
+	}
+	
+	/**
+	 * Boldfaces every column of the given row of the table in the DOM tree. The boldface (b)
+	 * tag appears directly under the td tag of every column of this row.
+	 * 
+	 * @param row Row to bold, first row is numbered 1 (not 0).
+	 */
+	public void boldRow(int row) {
+		/** COMPLETE THIS METHOD **/
+	}
+	
+	/**
+	 * Remove all occurrences of a tag from the DOM tree. If the tag is p, em, or b, all occurrences of the tag
+	 * are removed. If the tag is ol or ul, then All occurrences of such a tag are removed from the tree, and, 
+	 * in addition, all the li tags immediately under the removed tag are converted to p tags. 
+	 * 
+	 * @param tag Tag to be removed, can be p, em, b, ol, or ul
+	 */
+	public void removeTag(String tag) {
+		/** COMPLETE THIS METHOD **/
+	}
+	
+	/**
+	 * Adds a tag around all occurrences of a word in the DOM tree.
+	 * 
+	 * @param word Word around which tag is to be added
+	 * @param tag Tag to be added
+	 */
+	public void addTag(String word, String tag) {
+		/** COMPLETE THIS METHOD **/
+	}
+	
+	/**
+	 * Gets the HTML represented by this DOM tree. The returned string includes
+	 * new lines, so that when it is printed, it will be identical to the
+	 * input file from which the DOM tree was built.
+	 * 
+	 * @return HTML string, including new lines. 
+	 */
+	public String getHTML() {
+		StringBuilder sb = new StringBuilder();
+		getHTML(root, sb);
+		return sb.toString();
+	}
+	
+	private void getHTML(TagNode root, StringBuilder sb) {
+		for (TagNode ptr=root; ptr != null;ptr=ptr.sibling) {
+			if (ptr.firstChild == null) {
+				sb.append(ptr.tag);
+				sb.append("\n");
+			} else {
+				sb.append("<");
+				sb.append(ptr.tag);
+				sb.append(">\n");
+				getHTML(ptr.firstChild, sb);
+				sb.append("</");
+				sb.append(ptr.tag);
+				sb.append(">\n");	
+			}
+		}
+	}
+	
+}
